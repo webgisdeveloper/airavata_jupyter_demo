77fd893db12b9006695c2caa0ed4ea065c07aaaa
Jun Wang <wang208@indiana.edu>
2017-07-07 16:14:14 -0400

    note on install

--- dev_codes/airavata_client_test.py
+++ dev_codes/airavata_client_test.py
@@ -23,18 +23,19 @@ def main():
 	#goodex = ac.getexperimentbyid(exid)
 	#print(goodex)
 	# print("================")
-	ex =ac.sampleexperiment()
-	print(ex)
+	#ex =ac.sampleexperiment()
+	#print(ex)
 	
-	exid = ac.createexperiment(ex)
+	#exid = ac.createexperiment(ex)
 	
 	#exid ="GaussianTestJob01_951965a6-a87e-469c-ba23-c0256d564b9e"
 	#exid ="GaussianTestJob01_7b6080c9-6029-425d-bf89-9f9143f6caec"
 	#print(exid)
 	#print(ac.getexperiment(exid))
-	ac.launchexperiment(exid)
-	time.sleep(20)
-	print(ac.getexperimentstatus(exid))
+	#ac.launchexperiment(exid)
+	#time.sleep(20)
+	#print(ac.getexperimentstatus(exid))
+	exid = "test03_bd95f379-bc28-4300-95db-2e28df4377a4"
 	print(ac.getexperimentoutputs(exid))
 
 if __name__ == '__main__':
